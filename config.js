83cfe39441511981f63c2767ba1366480bf7c10a
Sujay <sujay@Sujays-MacBook-Pro.local>
2021-06-20 18:47:48 +0530

    new

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_mhYg2UXAT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4449303g041mhlfu1a5vf4tfms', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
